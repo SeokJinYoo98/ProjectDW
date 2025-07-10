3fcf4e24d162d37e418b913d8868f7f48f7e7aa7
OilStone <you8343@gmail.com>
2025-07-10 21:50:16 +0900

    feat: Character Base Class 추가.
    공통 기능으로 사용될 수 있는 기능은 추후 플레이어, 적, NPC를 구현하며 리팩토링 진행 예정

---
+++ Assets/Scripts/Character/Model/GameCharacterModel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GameCharacter.Model
+{
+    public class GameCharacterModel : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
