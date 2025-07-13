0d699d3c754b746c2c02f11deb2b336b830f5613
OilStone <you8343@gmail.com>
2025-07-13 11:21:28 +0900

    feat: 캐릭터 애니메이션 구현
    베이스 애니메이터 컨트롤러를 구현하고,
    ViewData에 오버라이드 컨트롤러를 저장,
    뷰 데이터를 변경함으로써 캐릭터 타입 또는 종류에 따른 애니메이션 구현 가능.
    
    + 뉴 인풋 시스템을 통해 간단한 애니메이션 테스트 기능 추가

---
+++ Assets/Scripts/Characters/PlayerCharacter/Controller/PlayerCharacterController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Character.Player
+{
+    [RequireComponent( typeof(Rigidbody2D))]
+    [RequireComponent( typeof(PlayerCharacterView))]
+    public class PlayerCharacterController : MonoBehaviour
+    {
+        private   PlayerInput          _input;
+        protected PlayerCharacterModel _model;
+        protected PlayerCharacterView  _view;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+        
+        public void Init(PlayerCharacterModel model, PlayerCharacterView view)
+        {
+            _model = model;
+            _view  = view;
+        }
+        void Start()
+        {
+            _view  = GetComponent<PlayerCharacterView>();
+            _input = GetComponent<PlayerInput>();
+
+            _input.actions["Num1"].performed += ctx => OnNumPressed( 1 );
+            _input.actions["Num2"].performed += ctx => OnNumPressed( 2 );
+            _input.actions["Num3"].performed += ctx => OnNumPressed( 3 );
+            _input.actions["Num4"].performed += ctx => OnNumPressed( 4 );
+
+        }
+        private void OnDestroy()
+        {
+            _input.actions["Num1"].performed -= ctx => OnNumPressed( 1 );
+            _input.actions["Num2"].performed -= ctx => OnNumPressed( 2 );
+            _input.actions["Num3"].performed -= ctx => OnNumPressed( 3 );
+            _input.actions["Num4"].performed -= ctx => OnNumPressed( 4 );
+        }
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        private void OnNumPressed(int num)
+        {
+            Debug.Log( $"{num}" );
+            _view.ChangeAnim( num );
+        }
+    }
+}
+
---
+++ Assets/Scripts/Characters/PlayerCharacter/View/PlayerCharacterView.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor.Animations;
+
+using UnityEngine;
+
+namespace Character.Player
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(AnimatorController))]
+    public class PlayerCharacterView : MonoBehaviour
+    {
+        [SerializeField] private PlayerCharacterViewData _viewData;
+        private Animator        _animator;
+        private SpriteRenderer  _sr;
+        public void SetViewData(PlayerCharacterViewData viewData)
+        {
+            _viewData = viewData;
+            ApplyViewData( );
+        }
+        private void ApplyViewData()
+        {
+            if (_viewData == null) return;
+
+            _sr.sprite = _viewData.DefaultSprite;
+            _animator.runtimeAnimatorController =  _viewData.AnimController;
+            ChangeAnim( 0 );
+        }
+        private void Awake()
+        {
+            _animator   = GetComponent<Animator>( );
+            _sr         = GetComponent<SpriteRenderer>( );
+            
+        }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            ApplyViewData( );
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        public void ChangeAnim(int num)
+        {
+            switch (num)
+            {
+                case 1: 
+                    SetMovement( 0f );
+                    break;
+                case 2: 
+                    SetMovement( 0.5f );
+                    break;
+                case 3: 
+                    SetMovement( 1.5f );
+                    break;
+                case 4:
+                    PlayDeath( );
+                    break;
+                default: break;
+            }
+        }
+        public void SetMovement(float speed)    => _animator.SetFloat( "Speed", speed );
+        public void PlayDeath()                 => _animator.SetTrigger( "Die" );
+    }
+}
