29376589941aa75c8a5bf1ce6c263beb47032144
OilStone <you8343@gmail.com>
2025-07-13 11:30:26 +0900

    chore: 애니메이션 테스트 코드 삭제
    PlayerInput을 통한 애니메이션 테스트 코드 삭제

--- Assets/Scripts/Characters/PlayerCharacter/Controller/PlayerCharacterController.cs
+++ Assets/Scripts/Characters/PlayerCharacter/Controller/PlayerCharacterController.cs
@@ -7,7 +7,6 @@ namespace Character.Player
     [RequireComponent( typeof(PlayerCharacterView))]
     public class PlayerCharacterController : MonoBehaviour
     {
-        private   PlayerInput          _input;
         protected PlayerCharacterModel _model;
         protected PlayerCharacterView  _view;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,20 +20,9 @@ namespace Character.Player
         void Start()
         {
             _view  = GetComponent<PlayerCharacterView>();
-            _input = GetComponent<PlayerInput>();
-
-            _input.actions["Num1"].performed += ctx => OnNumPressed( 1 );
-            _input.actions["Num2"].performed += ctx => OnNumPressed( 2 );
-            _input.actions["Num3"].performed += ctx => OnNumPressed( 3 );
-            _input.actions["Num4"].performed += ctx => OnNumPressed( 4 );
-
         }
         private void OnDestroy()
         {
-            _input.actions["Num1"].performed -= ctx => OnNumPressed( 1 );
-            _input.actions["Num2"].performed -= ctx => OnNumPressed( 2 );
-            _input.actions["Num3"].performed -= ctx => OnNumPressed( 3 );
-            _input.actions["Num4"].performed -= ctx => OnNumPressed( 4 );
         }
         // Update is called once per frame
         void Update()
@@ -43,7 +31,6 @@ namespace Character.Player
 
         private void OnNumPressed(int num)
         {
-            Debug.Log( $"{num}" );
             _view.ChangeAnim( num );
         }
     }
